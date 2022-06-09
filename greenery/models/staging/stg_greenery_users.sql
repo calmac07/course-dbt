815126f00ae80f39b0c9dce663a14e0e049a2d3d
calmac07 <callummccarthy07@gmail.com>
2022-06-09 11:05:05 +0000

    staging tables sql and schemas 001

---
+++ greenery/models/staging/stg_greenery_users.sql
@@ -0,0 +1,29 @@
+{{
+    config(
+        materialized = 'view'
+    )
+}}
+
+with users_sources as ( select * from {{ source('src_greenery' , 'users') }} )
+
+, renamed_recast as (
+
+    select 
+
+        user_id
+        , first_name  -- as user_guid
+        , last_name
+        , email
+        , phone_number
+        , created_at -- as created_at_utc
+        , updated_at -- as updated_at_utc
+        , address_id -- as address_guid
+
+    from users_sources
+
+    )
+
+    select * from renamed_recast 
+
+
+
